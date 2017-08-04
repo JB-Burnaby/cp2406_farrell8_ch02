9a8011282afbbbfd7608dea622e53bde317f4e57
Jonah Burnaby <jonah.burnaby@my.jcu.edu.au>
2017-08-04 18:24:49 +1000

    Initials

---
+++ DebuggingExercises/Initials.java
@@ -0,0 +1,10 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Initials {
+    public static void main(String[] args){
+        String fname = "J";
+        String mname = "B";
+        String lname = "B";
+        System.out.println(fname+  '.' + mname + '.' + lname + '.');
+    }
+}
